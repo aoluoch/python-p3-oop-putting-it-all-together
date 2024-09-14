7e53091f0dea40c04383af6c7310954321cbb3c7
Amos Oluoch <aoluoch271@gmail.com>
2024-09-15 01:21:20 +0300

    oop

--- lib/book.py
+++ lib/book.py
@@ -1,23 +1,34 @@
 #!/usr/bin/env python3
 
 class Book:
-    def __init__(self, title, page_count):
-        self.title = title
-        self.page_count = page_count
+    def __init__(self, title: str, page_count: int):
+        self._title = title
+        self._page_count = None
+        self.page_count = page_count  
 
     @property
-    def page_count(self):
+    def title(self) -> str:
+        return self._title
+
+    @title.setter
+    def title(self, value: str):
+        if isinstance(value, str):
+            self._title = value
+        else:
+            print("Title must be a string")
+
+    @property
+    def page_count(self) -> int:
         return self._page_count
 
     @page_count.setter
-    def page_count(self, value):
-        if not isinstance(value, int):
-            print("page_count must be an integer")
-        else:
+    def page_count(self, value: int):
+        if isinstance(value, int):
             self._page_count = value
+        else:
+            print("page_count must be an integer")
 
     def turn_page(self):
         print("Flipping the page...wow, you read fast!")
 
-    
-        
+    pass
--- lib/shoe.py
+++ lib/shoe.py
@@ -2,25 +2,32 @@
 
 class Shoe:
     def __init__(self, brand, size):
-        self.brand = brand
+        self._brand = brand
+        self._size = None
         self.size = size
 
     @property
-    def size(self):
+    def brand(self) -> str:
+        return self._brand
+
+    @brand.setter
+    def brand(self, value: str):
+        if isinstance(value, str):
+            self._brand = value
+        else:
+            print("Brand must be a string")
+
+    @property
+    def size(self) -> int:
         return self._size
 
     @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            print("size must be an integer")
-        else:
+    def size(self, value: int):
+        if isinstance(value, int):
             self._size = value
+        else:
+            print("size must be an integer")
 
     def cobble(self):
-        print("Your shoe has been repaired.")
- 
-
-
-
-
-
+        print("Your shoe is as good as new!")
+        self.condition = "New"
